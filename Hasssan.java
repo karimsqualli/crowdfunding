34f74827d7d1ca2e4e9f9d85f36d3267ff7414db
tahrisqalli <a.tahri.sqalli@gmail.com>
2017-11-25 11:55:57 +0000

    bba modif
    bba lfsser

--- Hasssan.java
+++ Hasssan.java
@@ -3,11 +3,15 @@ Class HasanLbogoss() {
 
 	int age = 31;
 	string nom = "hassan";
-<<<<<<< HEAD
-	int poid = 75;
+	
+	karim
 
-=======
-	bzdzhdzdbubd
-dzhdzudzhud
->>>>>>> 66570fd42af738d4ccd787c3e589377f349f71c5
+	hassan 
+
+	hicham 
+	
+	saad 
+	
+	ibtissam
+
 }
