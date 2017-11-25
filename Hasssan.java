e323161de1628e9b56cf21a7c9c9fab3eaa3e0f4
betty1233 <33979797+betty1233@users.noreply.github.com>
2017-11-25 12:21:46 +0000

    Revert "bba modif !!"
    modifier

--- Hasssan.java
+++ Hasssan.java
@@ -3,9 +3,12 @@ Class HasanLbogoss() {
 
 	int age = 31;
 	string nom = "hassan";
+<<<<<<< HEAD
 htfhf
 <<<<<<< HEAD
 	int poid = 75;
+=======
+>>>>>>> parent of fc75fb3... bba modif !!
 
 =======
 	bzdzhdzdbubd
