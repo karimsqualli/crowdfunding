a3f6f14c45831d6614229c93fa6ca9f5fd873a9b
Hamza Berrada <33551619+hamzaBerrada@users.noreply.github.com>
2018-01-28 01:45:44 +0000

    pb ServiceImp+ donation.xhtml reparation

--- src/org/mql/gc/actions/InsertCaseBean.java
+++ src/org/mql/gc/actions/InsertCaseBean.java
@@ -26,8 +26,8 @@ import org.apache.commons.io.IOUtils;
 public class InsertCaseBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Case caseObject;
-	private Service service;
-	private static final String BASE_STATIC_PATH = "D:/ProjetGL3/WebContent/static/";
+	private Service service;						//D:/ProjetGL3/WebContent/static/
+	private static final String BASE_STATIC_PATH = "C:/Users/SALAM/workspace/CrowdFunding`/crowdfunding/repoGit/WebContent/static/";
 	private static final String BASE_SERVER_STATIC_PATH = "./static/";
 	private static final String IMAGE_DIR = "images";
 	private static final String VIDEO_DIR = "videos";
